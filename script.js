19de2d12111c9352cf5ffdf8e7496aefdcefad30
Shawn Davis <smd031@gmail.com>
2023-07-05 13:27:13 -0500

    Starter HTML and timer logic

--- script.js
+++ script.js
@@ -13,13 +13,29 @@ var userInitials = document.getElementById("initials");
 
 startButton.addEventListener("click", startQuiz);
 
-function startQuiz({
+function startQuiz(){
     startButton.style.display = "none";
-
-})
+    timerInterval = setInterval(updateTimer, 1000);
+    nextQuestion();
+}
 
 // ----Timer---- //
 
+// Timer stolen from lesson 9
+function setTime() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function() {
+    time--;
+    timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
 
+    if(time === 0) {
+        // Stops execution of action at set interval
+        clearInterval(timerInterval);
+        // Calls function to create and append image
+        sendMessage();
+    }
 
-// 
+    }, 1000);
+}
+
+// ----Next Question---- //
