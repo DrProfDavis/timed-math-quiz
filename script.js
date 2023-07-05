0fb95870a8fa58a91013359025c2c9f7ea102cbc
Shawn Davis <smd031@gmail.com>
2023-07-05 12:35:34 -0500

    Added started code

---
+++ script.js
@@ -0,0 +1,25 @@
+// ----Variables---- //
+
+var time = 60;
+var currentQuestion = 0;
+
+var startButton = document.getElementById("start");
+var questionEl = document.getElementById("question");
+var answerChoices = document.getElementById("answer-choices");
+var submitButton = document.getElementById("submit");
+var userInitials = document.getElementById("initials");
+
+// ----Start Quiz---- //
+
+startButton.addEventListener("click", startQuiz);
+
+function startQuiz({
+    startButton.style.display = "none";
+
+})
+
+// ----Timer---- //
+
+
+
+// 
